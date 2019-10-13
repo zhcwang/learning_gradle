4b7d92f4c1d1812354f1e1240a56046b591011cb
zhcwang <zhengcaowang@163.com>
2019-10-13 10:48:02 +0800

    依赖管理

--- src/main/java/learning/gradle/todo/App.java
+++ src/main/java/learning/gradle/todo/App.java
@@ -1,16 +1,21 @@
 package learning.gradle.todo;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Scanner;
 
 public class App {
-
+    private static Logger logger = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int i = 0;
         while(++i > 0){
-            System.out.println(i + ". please input to item name:");
+            logger.info(i + ". please input to item name:");
             TodoItem item = new TodoItem(s.nextLine());
             System.out.println(item);
+
         }
     }
 }
